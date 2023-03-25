ce538b9bd4c5c10a65e4512bcb9e0e1ad3dabd66
ysy <2398581361@qq.com>
2023-03-25 13:57:31 +0800

    change

--- miniprogram/pages/shopdetail/shopdetail.js
+++ miniprogram/pages/shopdetail/shopdetail.js
@@ -1,80 +1,85 @@
+// pages/shopdetail/shopdetail.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    
+  show:false
   },
-  entershoplist(){
-   wx.navigateTo({
-     url: '../showlist/showlist',
-    
-   })
+  showcard(){
+    if (this.data.show == true) {
+      // 再点一下 取消选中
+      this.setData({
+        show: false
+      })
+    } else {
+      this.setData({
+        show:true
+      })
+    }
+  },
+  hidecard(){
+    if (this.data.show == true) {
+      // 再点一下 取消选中
+      this.setData({
+        show: false
+      })
+    } 
   },
   /**
    * 生命周期函数--监听页面加载
    */
-  entershopdetail(){
-wx.navigateTo({
-  url: '../shopdetail/shopdetail',
-})
-  },
-  onLoad: function (options) {
-    
+  onLoad(options) {
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-    
+  onReady() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-    if (typeof this.getTabBar === 'function' &&
-    this.getTabBar()) {
-    this.getTabBar().setData({
-      selected: 0
-    })
-  }
+  onShow() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-    
+  onHide() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-    
+  onUnload() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-    
+  onPullDownRefresh() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-    
+  onReachBottom() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-    
+  onShareAppMessage() {
+
   }
 })
